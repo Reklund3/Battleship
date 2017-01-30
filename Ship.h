bc1ca6e137dc69dc40c5024aedff67fcffcf7484
Reklund3 <robert.eklund3@gmail.com>
2017-01-29 19:12:09 -0600

    Update Ship.h

--- Ship.h
+++ Ship.h
@@ -25,6 +25,32 @@ public:
 				checkLoc = true;
 				if (_team == "") {
 					do {													//Parent loop running until the ship is not overlaping another ship
+#include "Board.h"
+#include <ctime>
+#include <random>
+
+using namespace std;
+
+uniform_int_distribution<int> o(0, 3), p(0, 9);							//defines the integer variables ranges for random number generation
+default_random_engine r(time(0));										//defines the algorithm used to generate random numbers. in this case the computers time clock was used
+
+class SHIPS : public Board {
+protected:
+	bool checkLoc;
+	const string shipName[4] = { "Cruiser", "Destroyer","Battleship","Carrier" }; //Array used for output statement to user when they are placing their ships on the board in the placeShip member function
+	const string dir[4] = { "n", "e", "s", "w" };									//Array used for determining a direction for the heading() member function, it converts the random number to a string output for the conditions within heading()
+	int _randDirection;											//declares a member variable used to store the random direction number used to reference a direction in the g_Dir array
+
+public:
+	SHIPS(string t) {
+		initializeGrid();
+		_team = t;
+		for (int z = 0; z < 4; z++)
+		{
+			do {
+				checkLoc = true;
+				if (_team == "") {
+					do {													//Parent loop running until the ship is not overlaping another ship
 						do {												//Loop getting users desired ship coordinates. This runs until the ship would be on the board and then exits to the parent loop
 							cout << "Please Enter " << shipName[shipSize - 1] << " Desired Direction of Travel as N, E, S, W" << endl;
 							getline(cin, direction);
@@ -56,23 +82,35 @@ public:
 				}
 				if (_team == "t") { x = 0; y = 0; }
 				for (int i = 0; i <= shipSize; i++) {										//this loop is used to take the users inputs and test make sure that the ship will not overlap another ship.
-					if (board[y][x] == "S") { checkLoc = false; }							//If any of the ships coordinates overlap another ship the g_CheckLoc bool is set to false so that the do while will run again
-					if (direction == "n" && board[y + i][x] == "S") { checkLoc = false; }
-					if (direction == "e" && board[y][x - i] == "S") { checkLoc = false; }	//in the event that a ship overlaps another already placed ship the m_CheckLoc bool is updated to false causing the main loop to run again
-					if (direction == "s" && board[y - i][x] == "S") { checkLoc = false; }
-					if (direction == "w" && board[y][x + i] == "S") { checkLoc = false; }
+					if (direction == "n" && board[y + i][x] == "S" || 
+						direction == "e" && board[y][x - i] == "S" || 
+						direction == "s" && board[y - i][x] == "S" || 
+						direction == "w" && board[y][x + i] == "S" ||
+						board[y][x] == "S") { checkLoc = false; }
 				}
 			} while (checkLoc == false);
 			for (int s = 0; s <= shipSize; s++) {
-				if (direction == "n") {
-					board[y + s][x] = "S";
-				}
-				if (direction == "e") {
-					board[y][x - s] = "S";
-				}
-				if (direction == "s") {
-					board[y - s][x] = "S";
-				}
+				if (direction == "n") {	board[y + s][x] = "S"; }
+				if (direction == "e") {	board[y][x - s] = "S"; }
+				if (direction == "s") {	board[y - s][x] = "S"; }
+				if (direction == "w") {	board[y][x + s] = "S"; }
+			} direction = ""; x = -1; y = -1; shipSize++;
+		}
+	}
+	void fireAtPlayer() {										//The function when called randomly generates coordinates and determines if the coordinates have either been used or are a miss or hit
+		do { xF = p(r); yF = p(r); 
+			} while (board[yF][xF] == "X" || board[yF][xF] == "O");
+		if (board[yF][xF] == " ") {								//If the coordantes dont hold a ship ("S") than the enemy missed and the condition statment is executed updating the g_PlayerBoard[][] array
+			std::cout << "The enemy fired at ( " << xF << " | " << yF << " ) missing your boats!\n" << std::endl;
+			board[yF][xF] = "O";
+		}
+		if (board[yF][xF] == "S") {								//If the coordantes hold a ship ("S") than the enemy hit and the condition statment is executed updating the g_PlayerBoard[][] array
+			std::cout << "Crap the enemy fired at ( " << xF << " | " << yF << " ) and hit a ship!\n" << std::endl;
+			board[yF][xF] = "X";
+			hitCount++;											//Adds one to the hitCountEnemy when the player has struck an enemy ship
+		}
+	}
+};
 				if (direction == "w") {
 					board[y][x + s] = "S";
 				}
